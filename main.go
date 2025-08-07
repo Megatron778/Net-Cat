77047ffd02b51f6728038d1308957d3fc11560c4
ybourazz <nite246135@gmail.com>
2025-08-07 11:49:15 +0100

    update 2.0

--- main.go
+++ main.go
@@ -1,74 +1,19 @@
 package main
 
 import (
-	"bufio"
 	"fmt"
-	"log"
 	"net"
-	"os"
-	"sync"
-)
-
-type Data struct {
-	Conn net.Conn
-	Name string
-}
-
-var (
-	Clients []*Data
-	Mutex sync.Mutex
+	"netcat/functions"
 )
 
 func main() {
-	Port := "8989"
-	if len(os.Args) == 2 {
-		Port = os.Args[1] 
-	} else if len(os.Args) > 2 {
-		fmt.Println("Usage : go run main.go <port>")
-		return
-	}
-
-	Listener, err := net.Listen("tcp", ":"+Port) 
+	listener, err := net.Listen("tcp", ":8989")
 	if err != nil {
 		fmt.Println(err)
 		return
 	}
-	fmt.Println("Server Listening on port :", Port)
+	fmt.Println("Server listening on port 8989")
 
-	defer Listener.Close()
+	functions.ConnectionCheck(listener)
 
-	for {
-		Connection, err := Listener.Accept()
-		if err != nil {
-			log.Println(err)
-		}
-		go HandlerConnection(Connection)
-	}
-}
-
-func HandlerConnection(Connection net.Conn) {
-	fmt.Println("The Clienr Connected")
-	defer Connection.Close()
-
-	_,err := Connection.Write([]byte("Server Connected \n Entre your name :"))
-	if err != nil {
-		log.Println(err)
-	}
-
-	reader := bufio.NewReader(Connection)
-	UserName, err := reader.ReadString('\n')
-	fmt.Println("Her name is" ,UserName)
-	_,err = Connection.Write([]byte("Welcome " + UserName))
-	if err != nil {
-		log.Println(err)
-	}
-
-	Client := &Data {
-		Conn: Connection,
-		Name: UserName,
-	}
-
-	Mutex.Lock()
-	Clients = append(Clients, Client)
-	Mutex.Unlock()
 }
